bfcbb54e568e3d7f307f06bd22ee756c9907c550
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-04-08 12:31:47 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach((el) => {
+  const item = document.createElement("li");
+  item.textContent = el;
+  item.classList.add("item");
+  list.append(item);
+});
