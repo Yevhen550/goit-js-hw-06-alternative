8f5fe9e0128fba4ed9a489fe25ab115c05b67ca4
Yevhen Liaskivskyi <benatanlev@icloud.com>
2024-04-13 15:02:53 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,4 +4,35 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-console.log("l");
+const boxEl = document.querySelector("#boxes");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const inputEl = document.querySelector("input");
+
+let boxSize = 30;
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const amo = +(amount.currentTarget.value = inputEl.value);
+  let newArr = [];
+
+  for (let i = 0; i < amo; i++) {
+    boxSize += 10;
+    const newDiv = document.createElement("div");
+    const color = getRandomHexColor();
+    newDiv.style.backgroundColor = color;
+    newDiv.style.width = `${boxSize}px`;
+    newDiv.style.height = `${boxSize}px`;
+    newArr.push(newDiv);
+  }
+
+  boxEl.append(...newArr);
+}
+
+function destroyBoxes() {
+  boxEl.innerHTML = "";
+  inputEl.value = "";
+  boxSize = 20;
+}
